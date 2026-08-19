39fd37dcc6ec8463ee39729ff93294e2b77a85cf
Jean Pierre MAC VEIGH <jpmacveigh@hotmail.fr>
2026-08-19 15:10:16 +0200

    Initial commit

---
+++ .history/alti_fl_20260819144425.py
@@ -0,0 +1,5 @@
+def alti_FL (FL,QNH,T_sol):
+  hauteur_standard_fl = float(FL)*100*0.3048
+  pression_fl_standard=1013.25 * (1-(0.0065*hauteur_standard_fl/288.15))**5.25588
+  h = (T_sol+273.15)/0.0065 * (1-(pression_fl_standard/QNH )**0.190263)
+  return(hauteur_standard_fl,pression_fl_standard,h)
---
+++ .history/altitude_FL_20260819144627.py
@@ -0,0 +1,59 @@
+import streamlit as st
+from alti_FL import alti_FL
+
+st.set_page_config(
+    page_title="Altitude d'un FL",
+    page_icon="✈️",
+    layout="centered"
+)
+
+st.title("✈️ Altitude géométrique d'un niveau de vol")
+
+st.write(
+    "Calcul de l'altitude géométrique correspondant à un FL "
+    "en fonction du QNH et de la température au sol."
+)
+
+
+# ------------------------------------------------------------
+# Entrées
+# ------------------------------------------------------------
+
+FL = st.number_input(
+    "FL",
+    min_value=1,
+    max_value=650,
+    value=115,
+    step=1
+)
+
+T_sol = st.number_input(
+    "Température au sol (°C)",
+    min_value=-60.0,
+    max_value=60.0,
+    value=35.0,
+    step=0.5
+)
+
+QNH = st.number_input(
+    "QNH (hPa)",
+    min_value=900.0,
+    max_value=1100.0,
+    value=1013.25,
+    step=0.1
+)
+
+# ------------------------------------------------------------
+# Calcul
+# ------------------------------------------------------------
+
+if st.button("Calculer", type="primary"):
+
+   altitude=alti_FL(FL,QNH,T_sol)
+
+    st.write("---")
+
+    st.write(f"**Altitude pression du FL{FL:03.0f} :** {Hp:.0f} m")
+    st.write(f"**QNH :** {QNH:.1f} hPa")
+    st.write(f"**Température au sol :** {T_sol:.1f} °C")
+
